1f3d9cb7c2fb9fd08ad13bdaa1c4cc687c141eb2
Noe Nieto <nnieto@noenieto.com>
2018-06-12 18:18:36 -0700

    Cleanup a little, remove dependencies on python_packages

--- device.py
+++ device.py
@@ -1,22 +1,175 @@
 import uuid
 import logging
+
+from ds import *
+
 log = logging.getLogger("Device")
+
+
+# TODO: Move this to the appropiate place
+def CreateNodeModel(device, region, model, expression):
+    """
+        Creates a node model
+    """
+    result = node_model(
+        device=device,
+        region=region,
+        name=model,
+        equation=expression
+    )
+    log.debug("NODEMODEL {d} {r} {m} \"{re}\"".format(d=device, r=region, m=model, re=result))
+
+
+# TODO: Move this to the appropiate place
+def InNodeModelList(device, region, model):
+    """
+    Checks to see if this node model is available on device and region
+    """
+    return model in get_node_model_list(device=device, region=region)
+
+
+# TODO: Move this to the appropiate place
+def InEdgeModelList(device, region, model):
+    """
+        Checks to see if this edge model is available on device and region
+    """
+    return model in get_edge_model_list(device=device, region=region)
+
+
+# TODO: Move this to the appropiate place
+def CreateEdgeModel(device, region, model, expression):
+    """
+    Creates an edge model
+    """
+    result = edge_model(device=device, region=region, name=model, equation=expression)
+    log.debug("EDGEMODEL {d} {r} {m} \"{re}\"".format(d=device, r=region, m=model, re=result))
+
+
+# TODO: Move this to the appropiate place
+def CreateNodeModelDerivative(device, region, model, expression, *args):
+    """
+    Create a node model derivative
+    """
+    for v in args:
+        CreateNodeModel(
+            device, region,
+            "{m}:{v}".format(m=model, v=v),
+            "simplify(diff({e},{v}))".format(e=expression, v=v)
+        )
+
+
+# TODO: Move this to the appropiate place
+def CreateSiliconPotentialOnly(device, region):
+    """
+        Creates the physical models for a Silicon region
+    """
+    if not InNodeModelList(device, region, "Potential"):
+        log("Creating Node Solution Potential")
+        CreateSolution(device, region, "Potential")
+    # require NetDoping
+    intrinsics = (
+        ("IntrinsicElectrons", "n_i*exp(Potential/V_t)"),
+        ("IntrinsicHoles", "n_i^2/IntrinsicElectrons"),
+        ("IntrinsicCharge", "kahan3(IntrinsicHoles, -IntrinsicElectrons, NetDoping)"),
+        ("PotentialIntrinsicCharge", "-ElectronCharge * IntrinsicCharge")
+    )
+    for name, eq in intrinsics:
+        CreateNodeModel(device, region, name, eq)
+        CreateNodeModelDerivative(device, region, name, eq, "Potential")
+
+    # TODO: Edge Average Model
+    electrics = (
+        ("ElectricField", "(Potential@n0-Potential@n1)*EdgeInverseLength"),
+        ("PotentialEdgeFlux", "Permittivity * ElectricField")
+    )
+    for name, eq in electrics:
+        CreateEdgeModel(device, region, name, eq)
+        CreateEdgeModelDerivatives(device, region, name, eq, "Potential")
+
+    equation(
+        device=device, region=region,
+        name="PotentialEquation", variable_name="Potential",
+        node_model="PotentialIntrinsicCharge",
+        edge_model="PotentialEdgeFlux",
+        variable_update="log_damp"
+    )
+
+
+# TODO: Move this to the appropiate place
+def CreateSiliconPotentialOnlyContact(device, region, contact, is_circuit=False):
+    """
+    Creates the potential equation at the contact
+    if is_circuit is true, than use node given by GetContactBiasName
+    """
+    # Means of determining contact charge
+    # Same for all contacts
+    if not InNodeModelList(device, region, "contactcharge_node"):
+        CreateNodeModel(device, region, "contactcharge_node", "ElectronCharge*IntrinsicCharge")
+    # TODO: This is the same as D-Field
+    if not InEdgeModelList(device, region, "contactcharge_edge"):
+        CreateEdgeModel(device, region, "contactcharge_edge", "Permittivity*ElectricField")
+        CreateEdgeModelDerivatives(device, region, "contactcharge_edge", "Permittivity*ElectricField", "Potential")
+
+
+# TODO: Move this to the appropiate place
+def CreateEdgeModelDerivatives(device, region, model, expression, variable):
+    """
+    Creates edge model derivatives
+    """
+    CreateEdgeModel(
+        device, region,
+        "{m}:{v}@n0".format(m=model, v=variable),
+        "simplify(diff({e}, {v}@n0))".format(e=expression, v=variable)
+    )
+    CreateEdgeModel(
+        device, region,
+        "{m}:{v}@n1".format(m=model, v=variable),
+        "simplify(diff({e}, {v}@n1))".format(e=expression, v=variable)
+    )
+
+
+# TODO: Move this to the appropiate place
+def DriftDiffusionInitialSolution(device, region, circuit_contacts=None):
+    # drift diffusion solution variables
+    CreateSolution(device, region, "Electrons")
+    CreateSolution(device, region, "Holes")
+
+    # create initial guess from dc only solution
+    set_node_values(device=device, region=region, name="Electrons", init_from="IntrinsicElectrons")
+    set_node_values(device=device, region=region, name="Holes", init_from="IntrinsicHoles")
+
+    # Set up equations
+    CreateSiliconDriftDiffusion(device, region)
+    for i in get_contact_list(device=device):
+        if circuit_contacts and i in circuit_contacts:
+            CreateSiliconDriftDiffusionAtContact(device, region, i, True)
+        else:
+            CreateSiliconDriftDiffusionAtContact(device, region, i)
+
+
+# TODO: Move this to the appropiate place
+def CreateSolution(device, region, name):
+    """
+        Creates solution variables
+        As well as their entries on each edge
+    """
+    node_solution(name=name, device=device, region=region)
+    edge_from_node_model(node_model=name, device=device, region=region)
 
 
 class Device(object):
     """docstring for Device"""
-    def __init__(self, name=None, mesh):
+
+    def __init__(self, name=None, mesh=None):
         self.name = name or 'device-%s' % str(uuid.uuid4())[:8]
         self.mesh = mesh
-        self.mesh.finalize()
-        create_device(mesh=self.mesh, device=self.name)
+        create_device(mesh=self.mesh.name, device=self.name)
 
     def _contact_bias_name(self, contact):
         return "{0}_bias".format(contact)
 
     def print_currents(self):
-        for c in self.mesh._contacts:
-            bias_name = self._contact_bias_name(c)
+        for c in self.mesh.contacts:
             e_current = get_contact_current(
                 device=device, contact=c, equation='ElectronContinuityEquation'
             )
@@ -32,30 +185,86 @@ class Device(object):
 
     def create_node_model(self, region, model, expression):
         result = node_model(device=self.name, region=region, name=model, equation=expression)
-        log.debug("NODEMODEL {d} {r} {m} \"{re}\"".format(d=device, r=region, m=model, re=result))
+        log.debug("NODEMODEL {d} {r} {m} \"{re}\"".format(d=self.name, r=region, m=model, re=result))
 
     def create_solution(self, region, name):
-      '''
+        '''
         Creates solution variables
         As well as their entries on each edge
-      '''
-      node_solution(name=name, device=self.name, region=region)
-      edge_from_node_model(node_model=name, device=self.name, region=region)
+        '''
+        node_solution(name=name, device=self.name, region=region)
+        edge_from_node_model(node_model=name, device=self.name, region=region)
 
-    def compute_initial_solution(self, region, circuit_contacts=None):
+    def solve(self, *args, **kwargs):
+        if not args and not kwargs:
+            self.initial_solution()
+        elif kwargs.get('type', '') == 'ramp':
+            kwargs['type'] = 'dc'
+            start = kwargs.pop('start')
+            stop = kwargs.pop('stop')
+            step = kwargs.pop('step')
+            contact = kwargs.pop('contact')
+            for v in range(start, stop, step):
+                set_parameter(
+                    device=self.name,
+                    name=self._contact_bias_name(contact),
+                    value=v
+                )
+                solve(**kwargs)
+                self.print_currents()
+        else:
+            solve(*args, **kwargs)
+
+    def initial_solution(self):
+        region = self.mesh.regions[0]
         # Create Potential, Potential@n0, Potential@n1
-        CreateSolution(device, region, "Potential")
+        CreateSolution(self.name, region, "Potential")
 
         # Create potential only physical models
-        CreateSiliconPotentialOnly(device, region)
+        # TODO: move to materials, relate region with material
+        CreateSiliconPotentialOnly(self.name, region)
 
         # Set up the contacts applying a bias
-        for i in get_contact_list(device=device):
-            if circuit_contacts and i in circuit_contacts:
-                    CreateSiliconPotentialOnlyContact(device, region, i, True)
-            else:
-                ###print "FIX THIS"
-                ### it is more correct for the bias to be 0, and it looks like there is side effects
-                set_parameter(device=device, name=GetContactBiasName(i), value=0.0)
-                CreateSiliconPotentialOnlyContact(device, region, i)
+        # TODO: Try to use self.contacts instead
+        # it is more correct for the bias to be 0, and it looks like there is side effects
 
+        for c in self.mesh.contacts:
+            set_parameter(device=self.name, name=self._contact_bias_name(c), value=0.0)
+            # TODO: move to models module
+            CreateSiliconPotentialOnlyContact(self.name, region, c)
+
+    def create_solution_variable(self, name):
+        """
+        Creates solution variables
+        As well as their entries on each edge
+        """
+        node_solution(name=name, device=self.name, region=self.mesh.regions[0])
+        edge_from_node_model(node_model=name, device=self.name, region=self.mesh.regions[0])
+
+    def drift_diffusion_initial_solution(self):
+        # TODO: move it to somewhere else
+        # drift diffusion solution variables
+        create_solution_variable("Electrons")
+        create_solution_variable("Holes")
+
+        # Create initial guess from DC only solution
+        set_node_values(
+            device=self.name,
+            region=self.mesh.regions[0],
+            name="Electrons",
+            init_from="IntrinsicElectrons"
+        )
+        set_node_values(
+            device=self.name,
+            region=self.mesh.regions[0],
+            name="Holes",
+            init_from="IntrinsicHoles"
+        )
+
+        # Set up equations
+        CreateSiliconDriftDiffusion(self.name, self.mesh.regions[0])
+        for c in self.mesh.contacts:
+            CreateSiliconDriftDiffusionAtContact(device, region, c)
+
+    def export(self, filename, format):
+        write_devices(file=filename, type=format)
--- solar_cell.py
+++ solar_cell.py
@@ -2,46 +2,38 @@ import mesh
 import device
 import materials
 
-
 mesh = mesh.Mesh('Cell Mesh')
 mesh.add_line(pos=0, ps=1e-7, tag='top')
 mesh.add_line(pos=0.5e-5, ps=1e-9, tag='middle')
 mesh.add_line(pos=1e-5, ps=1e-7, tag='bottom')
 mesh.add_contact(name='top', tag='top', material=materials.Metals.generic)
 mesh.add_contact(name='bottom', tag='bottom', material=materials.Metals.generic)
+
 substrate = 'Cell Substrate'
 mesh.add_region(
     name=substrate,
-    material=materials.Semiconductors.Silicon(taup=1e-8, taun=1e-8),
-    tag1='top', tag2='bot')
-pv_device = device.Device('SolarCell', mesh=mesh)
-pv_device.create_node_model(substrate, 'Acceptors', '1.0e18*step(0.5e-5-x)')
-pv_device.create_node_model(substrate, 'Donors', '1.0e18*step(x-0.5e-5)')
-pv_device.create_node_model(substrate, 'NetDoping', 'Donors-Acceptors')
+    material=materials.Silicon(taup=1e-8, taun=1e-8),
+    tag1='top', tag2='bottom'
+)
+mesh.finalize()
 
-# ?? print_node_values(device=pv_device, region=region, name='NetDoping')
+pv_cell = device.Device('SolarCell', mesh=mesh)
+pv_cell.create_node_model(substrate, 'Acceptors', '1.0e16*step(0.5e-5-x)')
+pv_cell.create_node_model(substrate, 'Donors', '1.0e18*step(x-0.5e-5)')
+pv_cell.create_node_model(substrate, 'NetDoping', 'Donors-Acceptors')
+pv_cell.solve()
+pv_cell.solve(type='dc', absolute_error=1.0, relative_error=1e-10, maximum_iterations=30)
 
-diode_common.InitialSolution(pv_device, region)
+# TODO: move this to another place, maybe solution module?
+pv_cell.drift_diffusion_initial_solution()
 
+# Drift diffusion simulation at equilibrium
+pv_cell.solve(type='dc', absolute_error=1e10, relative_error=1e-10, maximum_iterations=30)
 
-# Initial DC solution
-solve(type='dc', absolute_error=1.0, relative_error=1e-10, maximum_iterations=30)
+# Ramp the bias to 0.5 Volts
+pv_cell.solve(type='ramp', start=0.0, stop=0.5, step=0.1,
+              absolute_error=1e10,
+              relative_error=1e-10,
+              maximum_iterations=30)
 
-diode_common.DriftDiffusionInitialSolution(device, region)
-###
-### Drift diffusion simulation at equilibrium
-###
-solve(type='dc', absolute_error=1e10, relative_error=1e-10, maximum_iterations=30)
-
-
-####
-#### Ramp the bias to 0.5 Volts
-####
-v = 0.0
-while v < 0.51:
-    set_parameter(device=device, name=GetContactBiasName('top'), value=v)
-    solve(type='dc', absolute_error=1e10, relative_error=1e-10, maximum_iterations=30)
-    pv_device.print_currents()
-    v += 0.1
-
-write_devices(file='diode_1d.dat', type='tecplot')
+pv_cell.export(file='pv_cell.dat', type='tecplot')

5444b343979fc7786023f2acfb1c7b4c59c98354
Noe Nieto <nnieto@noenieto.com>
2018-07-05 13:20:48 -0700

    Parser for the devsim file format. Not my best work, but works

---
+++ tracey/parser.py
@@ -0,0 +1,73 @@
+#!/bin/env python3
+
+
+class Device(object):
+    """
+    Simple object contains the coordinates
+    Should probably be replace with numpy's dtype and loadtxt
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self.regions = {}
+        with open(filename, 'r') as fp:
+            line = fp.readline().strip()
+            if line.startswith('begin_device'):
+                _, n = line.split()
+                self.name = n.strip('"')
+                line = fp.readline().strip()
+                while line != 'end_device':
+                    if line.startswith('begin_coordinates'):
+                        self.coordinates = []
+                        line = fp.readline().strip()
+                        while line != 'end_coordinates':
+                            self.coordinates.append([float(i) for i in line.strip().split()])
+                            line = fp.readline().strip()
+                    elif line.startswith('begin_region'):
+                        _, rname, rmaterial = line.split()
+                        rname = rname.strip('"')
+                        self.regions[rname] = {
+                            'material': rmaterial.strip('"'),
+                            'nodes': [],
+                            'node_solutions': {},
+                            'edges': [],
+                            'edge_solutions': {},
+                        }
+                        line = fp.readline().strip()
+                        while line != 'end_region':
+                            if line.startswith('begin_nodes'):
+                                line = fp.readline().strip()
+                                while line != 'end_nodes':
+                                    self.regions[rname]['nodes'].append(int(line.strip()))
+                                    line = fp.readline().strip()
+                            elif line.startswith('begin_edges'):
+                                line = fp.readline().strip()
+                                while line != 'end_edges':
+                                    self.regions[rname]['edges'].append(
+                                        [int(e) for e in line.split()]
+                                    )
+                                    line = fp.readline().strip()
+                            elif line.startswith('begin_node_solution'):
+                                _, sname = line.split()
+                                sname = sname.strip('"')
+                                self.regions[rname]['node_solutions'][sname] = []
+                                line = fp.readline().strip()
+                                while line != 'end_node_solution':
+                                    self.regions[rname]['node_solutions'][sname].append(float(line.strip()))
+                                    line = fp.readline().strip()
+                            elif line.startswith('begin_edge_solution'):
+                                _, sname = line.split()
+                                sname = sname.strip('"')
+                                self.regions[rname]['edge_solutions'][sname] = []
+                                line = fp.readline().strip()
+                                while line != 'end_edge_solution':
+                                    self.regions[rname]['edge_solutions'][sname].append(float(line.strip()))
+                                    line = fp.readline().strip()
+                            line = fp.readline().strip()
+                    line = fp.readline().strip()
+    def __str__(self):
+        return 'Device "{}" ({} regions)'.format(self.name, len(self.regions))
+
+
+if __name__ == '__main__':
+    d = Device('../resistor.dat')
+    print(d)

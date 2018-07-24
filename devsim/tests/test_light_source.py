641b2b18a16fe1e04d18f295cf162720a0ec2053
Noe Nieto <nnieto@noenieto.com>
2018-07-24 12:57:03 -0700

    Basic functionality of light source AM0

--- devsim/tests/test_light_source.py
+++ devsim/tests/test_light_source.py
@@ -1,12 +1,11 @@
 import unittest
 
 
-
 class LightsourceTestCase(unittest.TestCase):
     def test_am0(self):
         from devsim.light_sources import AM0
         src = AM0()
         self.assertEqual(src.lambda_min, 280)
         self.assertEqual(src.lambda_max, 4000)
-        self.assertEqual(src.irradiance(280), 4000)
-        self.assertEqual(src.irradiance(4000), 4000)
+        self.assertEqual(src.irradiance(280), 8.2e-2)
+        self.assertEqual(src.irradiance(4000), 8.68e-3)

c3e551895c77fc8b1a9a133511ac75b91257cede
Noe Nieto <nnieto@noenieto.com>
2018-07-24 18:00:05 -0700

    AM0 Windowing

--- devsim/tests/test_light_source.py
+++ devsim/tests/test_light_source.py
@@ -16,12 +16,14 @@ class LightsourceTestCase(unittest.TestCase):
         self.assertEqual(src.irradiance(280.75), 0.1245)
         self.assertEqual(src.irradiance(3926.25), 0.00938)
 
-    def test_am0_subset(self):
+    def test_am0_window(self):
         from devsim.light_sources import AM0
         src = AM0(lambda_min=500, lambda_max=1000)
         self.assertEqual(src.lambda_min, 500)
         self.assertEqual(src.lambda_max, 1000)
-        self.assertEqual(src.irradiance(500), 8.2e-2)
-        self.assertEqual(src.irradiance(1000), 8.68e-3)
-
+        self.assertEqual(src.irradiance(0), 0.0)
+        self.assertEqual(src.irradiance(499), 0.0)
+        self.assertEqual(src.irradiance(500), 1.92)
+        self.assertEqual(src.irradiance(1000), 0.743)
+        self.assertEqual(src.irradiance(1001), 0.0)
 

44e401b4f57585a9990c399809e6714827adcb9a
Noe Nieto <nnieto@noenieto.com>
2018-07-27 00:22:05 -0700

    Convert solar cell to a testcase class. Fix resistor test

--- devsim/tests/test_solar_cell.py
+++ devsim/tests/test_solar_cell.py
@@ -1,39 +1,45 @@
-from devsim import mesh, device, materials
+import unittest
 
 
-mesh = mesh.Mesh('Cell Mesh')
-# default units are micrometers
-# (position, spacing, tag)
-mesh.add_line(position=0.00, spacing=0.01, tag='anode')
-mesh.add_line(position=10.00, spacing=1.0, tag='cathode')
-mesh.add_contact(name='anode', tag='anode', material=materials.Metals.generic)
-mesh.add_contact(name='cathode', tag='cathode', material=materials.Metals.generic)
+from devsim.mesh import Mesh
+from devsim.device import Device
+from devsim import materials
 
-substrate = 'Cell Substrate'
-mesh.add_region(
-    name=substrate,
-    material=materials.Silicon(taup=1e-8, taun=1e-8),
-    tag1='anode', tag2='cathode'
-)
-mesh.finalize()
+class SolarCellTestCase(unittest.TestCase):
+    def setUp(self):
+        mesh = Mesh('Solar Cell Mesh')
+        # default units are micrometers
+        # (position, spacing, tag)
+        mesh.add_line(position=0.00, spacing=0.01, tag='anode')
+        mesh.add_line(position=10.00, spacing=1.0, tag='cathode')
+        mesh.add_contact(name='anode', tag='anode', material=materials.Metals.generic)
+        mesh.add_contact(name='cathode', tag='cathode', material=materials.Metals.generic)
 
-pv_cell = device.Device('SolarCell', mesh=mesh)
-pv_cell.create_node_model(substrate, 'Acceptors', '1.0e16*step(0.5e-5-x)')
-pv_cell.create_node_model(substrate, 'Donors', '1.0e18*step(x-0.5e-5)')
-pv_cell.create_node_model(substrate, 'NetDoping', 'Donors-Acceptors')
-pv_cell.solve()
-pv_cell.solve(type='dc', absolute_error=1.0, relative_error=1e-10, maximum_iterations=30)
+        substrate = 'Cell Substrate'
+        mesh.add_region(
+            name=substrate,
+            material=materials.Silicon(taup=1e-8, taun=1e-8),
+            tag1='anode', tag2='cathode'
+        )
+        mesh.finalize()
 
-# TODO: move this to another place, maybe solution module?
-pv_cell.drift_diffusion_initial_solution()
+        pv_cell = device.Device('SolarCell', mesh=mesh)
+        pv_cell.create_node_model(substrate, 'Acceptors', '1.0e16*step(0.5e-5-x)')
+        pv_cell.create_node_model(substrate, 'Donors', '1.0e18*step(x-0.5e-5)')
+        pv_cell.create_node_model(substrate, 'NetDoping', 'Donors-Acceptors')
+        pv_cell.solve()
+        pv_cell.solve(type='dc', absolute_error=1.0, relative_error=1e-10, maximum_iterations=30)
 
-# Drift diffusion simulation at equilibrium
-pv_cell.solve(type='dc', absolute_error=1e10, relative_error=1e-10, maximum_iterations=30)
+        # TODO: move this to another place, maybe solution module?
+        pv_cell.drift_diffusion_initial_solution()
 
-# Ramp the bias to 0.5 Volts
-pv_cell.solve(type='ramp', start=0.0, stop=0.5, step=0.1,
-              absolute_error=1e10,
-              relative_error=1e-10,
-              maximum_iterations=30)
+        # Drift diffusion simulation at equilibrium
+        pv_cell.solve(type='dc', absolute_error=1e10, relative_error=1e-10, maximum_iterations=30)
 
-pv_cell.export(file='pv_cell.dat', type='tecplot')
+        # Ramp the bias to 0.5 Volts
+        pv_cell.solve(type='ramp', start=0.0, stop=0.5, step=0.1,
+                      absolute_error=1e10,
+                      relative_error=1e-10,
+                      maximum_iterations=30)
+
+        pv_cell.export(file='pv_cell.dat', type='tecplot')
